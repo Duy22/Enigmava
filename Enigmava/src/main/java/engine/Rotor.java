f1187bec013679e3ae76ecf776f5c454bc44e9c2
Duy22 <fabienduytran@hotmail.fr>
2015-11-24 13:53:43 -0500

    Completed Rotor class.
    -moveRight() and moveLeft() have been written.

--- Enigmava/src/main/java/engine/Rotor.java
+++ Enigmava/src/main/java/engine/Rotor.java
@@ -46,10 +46,31 @@ public class Rotor {
 	}
 	
 	public void moveLeft(){
+		int upperNum = this.getUpperLane(0);
+		for (int i=0;i<25;i++){
+			this.setUpperLane(i, this.getUpperLane(i+1));
+			this.setUpperLane(25, upperNum);
+		}
 		
+		int lowerNum = this.getLowerLane(0);
+		for (int i=0;i<25;i++){
+			this.setLowerLane(i, this.getLowerLane(i+1));
+			this.setLowerLane(25, lowerNum);
+		}
 	}
 	
 	public void moveRight(){
+		int upperNum = this.getUpperLane(25);
+		for (int i=25;i<0;i--){
+			this.setUpperLane(i, i-1);
+			this.setUpperLane(0, upperNum);
+		}
+		
+		int lowerNum = this.getLowerLane(25);
+		for (int i=25;i<0;i--){
+			this.setLowerLane(i, i-1);
+			this.setLowerLane(0, upperNum);
+		}
 		
 	}
 }
