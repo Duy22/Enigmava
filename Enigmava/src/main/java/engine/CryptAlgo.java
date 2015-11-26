77c50d0dafcea0402e89900a016f10286f8d5e09
Duy22 <fabienduytran@hotmail.fr>
2015-11-26 18:21:28 -0500

    Initialization of the three rotors in CryptAlgo. -initRotor1(); -initRotor2(); -initRotor3();

--- Enigmava/src/main/java/engine/CryptAlgo.java
+++ Enigmava/src/main/java/engine/CryptAlgo.java
@@ -46,6 +46,185 @@ public class CryptAlgo {
 		this.decryptedMessage = s;
 	}
 	
+	public void initReflector(){
+		int decrement = 27;
+		for (int i=0; i<26; i++){
+			this.reflector.setNum(i, decrement-2);
+		}
+	}
+	
+	public void initRotor1(){
+		//upper lane settings
+		this.rotorList.get(0).setUpperLane(0, 17);
+		this.rotorList.get(0).setUpperLane(1, 4);
+		this.rotorList.get(0).setUpperLane(2, 19);
+		this.rotorList.get(0).setUpperLane(3, 21);
+		this.rotorList.get(0).setUpperLane(4, 7);
+		this.rotorList.get(0).setUpperLane(5, 11);
+		this.rotorList.get(0).setUpperLane(6, 3);
+		this.rotorList.get(0).setUpperLane(7, -5);
+		this.rotorList.get(0).setUpperLane(8, 7);
+		this.rotorList.get(0).setUpperLane(9, 9);
+		this.rotorList.get(0).setUpperLane(10, -10);
+		this.rotorList.get(0).setUpperLane(11, 9);
+		this.rotorList.get(0).setUpperLane(12, 17);
+		this.rotorList.get(0).setUpperLane(13, 6);
+		this.rotorList.get(0).setUpperLane(14, -6);
+		this.rotorList.get(0).setUpperLane(15, -2);
+		this.rotorList.get(0).setUpperLane(16, -4);
+		this.rotorList.get(0).setUpperLane(17, -7);
+		this.rotorList.get(0).setUpperLane(18, -12);
+		this.rotorList.get(0).setUpperLane(19, -5);
+		this.rotorList.get(0).setUpperLane(20, 3);
+		this.rotorList.get(0).setUpperLane(21, 4);
+		this.rotorList.get(0).setUpperLane(22, -21);
+		this.rotorList.get(0).setUpperLane(23, -16);
+		this.rotorList.get(0).setUpperLane(24, -2);
+		this.rotorList.get(0).setUpperLane(25, -21);
+		//lower lane settings
+		this.rotorList.get(0).setLowerLane(0, 10);
+		this.rotorList.get(0).setLowerLane(1, 21);
+		this.rotorList.get(0).setLowerLane(2, -5);
+		this.rotorList.get(0).setLowerLane(3, -17);
+		this.rotorList.get(0).setLowerLane(4, 21);
+		this.rotorList.get(0).setLowerLane(5, -4);
+		this.rotorList.get(0).setLowerLane(6, 12);
+		this.rotorList.get(0).setLowerLane(7, 16);
+		this.rotorList.get(0).setLowerLane(8, 6);
+		this.rotorList.get(0).setLowerLane(9, -3);
+		this.rotorList.get(0).setLowerLane(10, 7);
+		this.rotorList.get(0).setLowerLane(11, -7);
+		this.rotorList.get(0).setLowerLane(12, 4);
+		this.rotorList.get(0).setLowerLane(13, 2);
+		this.rotorList.get(0).setLowerLane(14, 5);
+		this.rotorList.get(0).setLowerLane(15, -7);
+		this.rotorList.get(0).setLowerLane(16, -11);
+		this.rotorList.get(0).setLowerLane(17, -17);
+		this.rotorList.get(0).setLowerLane(18, -9);
+		this.rotorList.get(0).setLowerLane(19, -6);
+		this.rotorList.get(0).setLowerLane(20, -9);
+		this.rotorList.get(0).setLowerLane(21, -19);
+		this.rotorList.get(0).setLowerLane(22, 2);
+		this.rotorList.get(0).setLowerLane(23, -3);
+		this.rotorList.get(0).setLowerLane(24, -21);
+		this.rotorList.get(0).setLowerLane(25, 4);
+	}
+	
+	public void initRotor2(){
+		//upper lane settings
+		this.rotorList.get(1).setUpperLane(0, 25);
+		this.rotorList.get(1).setUpperLane(1, 7);
+		this.rotorList.get(1).setUpperLane(2, 17);
+		this.rotorList.get(1).setUpperLane(3, -3);
+		this.rotorList.get(1).setUpperLane(4, 13);
+		this.rotorList.get(1).setUpperLane(5, 19);
+		this.rotorList.get(1).setUpperLane(6, 12);
+		this.rotorList.get(1).setUpperLane(7, 3);
+		this.rotorList.get(1).setUpperLane(8, -1);
+		this.rotorList.get(1).setUpperLane(9, 11);
+		this.rotorList.get(1).setUpperLane(10, 5);
+		this.rotorList.get(1).setUpperLane(11, -5);
+		this.rotorList.get(1).setUpperLane(12, -7);
+		this.rotorList.get(1).setUpperLane(13, 10);
+		this.rotorList.get(1).setUpperLane(14, -2);
+		this.rotorList.get(1).setUpperLane(15, 1);
+		this.rotorList.get(1).setUpperLane(16, -2);
+		this.rotorList.get(1).setUpperLane(17, 4);
+		this.rotorList.get(1).setUpperLane(18, -17);
+		this.rotorList.get(1).setUpperLane(19, -8);
+		this.rotorList.get(1).setUpperLane(20, -16);
+		this.rotorList.get(1).setUpperLane(21, -18);
+		this.rotorList.get(1).setUpperLane(22, -9);
+		this.rotorList.get(1).setUpperLane(23, -1);
+		this.rotorList.get(1).setUpperLane(24, -11);
+		this.rotorList.get(1).setUpperLane(25, -16);
+		//lower lane settings
+		this.rotorList.get(1).setLowerLane(0, 3);
+		this.rotorList.get(1).setLowerLane(1, 17);
+		this.rotorList.get(1).setLowerLane(2, 22);
+		this.rotorList.get(1).setLowerLane(3, 18);
+		this.rotorList.get(1).setLowerLane(4, 16);
+		this.rotorList.get(1).setLowerLane(5, 7);
+		this.rotorList.get(1).setLowerLane(6, 5);
+		this.rotorList.get(1).setLowerLane(7, 1);
+		this.rotorList.get(1).setLowerLane(8, -7);
+		this.rotorList.get(1).setLowerLane(9, 16);
+		this.rotorList.get(1).setLowerLane(10, -3);
+		this.rotorList.get(1).setLowerLane(11, 8);
+		this.rotorList.get(1).setLowerLane(12, 2);
+		this.rotorList.get(1).setLowerLane(13, 9);
+		this.rotorList.get(1).setLowerLane(14, 2);
+		this.rotorList.get(1).setLowerLane(15, -5);
+		this.rotorList.get(1).setLowerLane(16, -1);
+		this.rotorList.get(1).setLowerLane(17, -13);
+		this.rotorList.get(1).setLowerLane(18, -12);
+		this.rotorList.get(1).setLowerLane(19, -17);
+		this.rotorList.get(1).setLowerLane(20, -11);
+		this.rotorList.get(1).setLowerLane(21, -4);
+		this.rotorList.get(1).setLowerLane(22, 1);
+		this.rotorList.get(1).setLowerLane(23, -10);
+		this.rotorList.get(1).setLowerLane(24, -19);
+		this.rotorList.get(1).setLowerLane(25, -25);
+	}
+	
+	public void initRotor3(){
+		
+		//upper lane settings
+		this.rotorList.get(2).setUpperLane(0, 12);
+		this.rotorList.get(2).setUpperLane(1, -1);
+		this.rotorList.get(2).setUpperLane(2, 23);
+		this.rotorList.get(2).setUpperLane(3, 10);
+		this.rotorList.get(2).setUpperLane(4, 2);
+		this.rotorList.get(2).setUpperLane(5, 14);
+		this.rotorList.get(2).setUpperLane(6, 5);
+		this.rotorList.get(2).setUpperLane(7, -5);
+		this.rotorList.get(2).setUpperLane(8, 9);
+		this.rotorList.get(2).setUpperLane(9, -2);
+		this.rotorList.get(2).setUpperLane(10, -13);
+		this.rotorList.get(2).setUpperLane(11, 10);
+		this.rotorList.get(2).setUpperLane(12, -2);
+		this.rotorList.get(2).setUpperLane(13, -8);
+		this.rotorList.get(2).setUpperLane(14, 10);
+		this.rotorList.get(2).setUpperLane(15, -6);
+		this.rotorList.get(2).setUpperLane(16, 6);
+		this.rotorList.get(2).setUpperLane(17, -16);
+		this.rotorList.get(2).setUpperLane(18, 2);
+		this.rotorList.get(2).setUpperLane(19, -1);
+		this.rotorList.get(2).setUpperLane(20, -7);
+		this.rotorList.get(2).setUpperLane(21, -5);
+		this.rotorList.get(2).setUpperLane(22, -14);
+		this.rotorList.get(2).setUpperLane(23, -9);
+		this.rotorList.get(2).setUpperLane(24, -20);
+		this.rotorList.get(2).setUpperLane(25, -10);
+		//lower lane settings
+		this.rotorList.get(2).setLowerLane(0, 1);
+		this.rotorList.get(2).setLowerLane(1, 16);
+		this.rotorList.get(2).setLowerLane(2, 5);
+		this.rotorList.get(2).setLowerLane(3, 17);
+		this.rotorList.get(2).setLowerLane(4, 20);
+		this.rotorList.get(2).setLowerLane(5, 8);
+		this.rotorList.get(2).setLowerLane(6, -2);
+		this.rotorList.get(2).setLowerLane(7, 2);
+		this.rotorList.get(2).setLowerLane(8, 14);
+		this.rotorList.get(2).setLowerLane(9, 6);
+		this.rotorList.get(2).setLowerLane(10, 2);
+		this.rotorList.get(2).setLowerLane(11, -5);
+		this.rotorList.get(2).setLowerLane(12, -12);
+		this.rotorList.get(2).setLowerLane(13, -10);
+		this.rotorList.get(2).setLowerLane(14, 9);
+		this.rotorList.get(2).setLowerLane(15, 10);
+		this.rotorList.get(2).setLowerLane(16, 5);
+		this.rotorList.get(2).setLowerLane(17, -9);
+		this.rotorList.get(2).setLowerLane(18, 1);
+		this.rotorList.get(2).setLowerLane(19, -14);
+		this.rotorList.get(2).setLowerLane(20, -2);
+		this.rotorList.get(2).setLowerLane(21, -10);
+		this.rotorList.get(2).setLowerLane(22, -6);
+		this.rotorList.get(2).setLowerLane(23, 13);
+		this.rotorList.get(2).setLowerLane(24, -10);
+		this.rotorList.get(2).setLowerLane(25, -23);
+	}
+	
 	public String encrypt(String s){
 		
 		return s;
